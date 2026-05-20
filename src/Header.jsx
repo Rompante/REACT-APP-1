be65ddfd3c79506f843389e97deda5fe321b0e1a
Rompante <a14866@oficina.pt>
2026-05-20 10:55:05 +0000

    finilizar o footer e o header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import './Header.css'
 
 function Header (){
     return (
         <div>
-            <img src={heroImg} className="base" width="170" height="179" alt="" />
+            <logo><img src={heroImg}  alt="Hero Vite" /></logo>
+            <nav>
+                <a href="/home">HOME</a>
+                <a href="/contactos">CONTACTOS</a>
+            </nav>
         </div> 
     )
 }
