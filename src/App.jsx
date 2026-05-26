374c3de15cf88fc2ede23f5a34b8f60768affd7f
Rompante <a14866@oficina.pt>
2026-05-26 09:41:58 +0000

    Parte1 –Ex5-React

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,18 @@ import Alerta from './Alerta';
 import Contador from './Contador';
 import Formulario from './Formulario'
 import Tarefas from './Tarefas'
+import Notas from './Notas'
 
 // Um componente React é uma função que devolve JSX (JavaScript + HTML)
 function App() {
   return (
     <div className="container mt-4">
-      <h1 className="text-center text-primary">Olá React!</h1>
+      {/*<h1 className="text-center text-primary">Olá React!</h1>
       <p className="lead text-center">
         Este é o teu primeiro componente React.
       </p>
       {/* Evento onClick em React usa camelCase e chama uma função */}
-      <button
+      {/*<button
         className="btn btn-success d-block mx-auto"
         onClick={() => alert('Olá, Rompante bola de ouro!')}
       >
@@ -28,9 +29,9 @@ function App() {
 
       <Contador />
 
-      <h1 className="text-center">Componentes e Props</h1>
+      <h1 className="text-center">Componentes e Props</h1>*/}
       {/* Cada componente recebe props diferentes */}
-      <Mensagem mytext="Bem-vindo à tua primeira aplicação React!" />
+      {/*<Mensagem mytext="Bem-vindo à tua primeira aplicação React!" />
       <Mensagem mytext="Os componentes permitem reutilizar código!" />
 
       <Alerta tipo="success" texto="O componente Alerta está a funcionar"/>
@@ -38,7 +39,11 @@ function App() {
       <Alerta tipo="warning" texto="O componente Alerta está a funcionar"/>
 
       <Formulario />
-      <Tarefas />
+      <Tarefas />*/}
+
+      <Notas />
+
+
     </div>
   );
 }
---
+++ src/Notas.jsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+
+
+
+function Notas() {
+
+    const [formData, setFormData] = useState({
+        nome: '',
+        disciplina: '',
+        notasTestes: '',
+        testes: '',
+        notasTrabalhos: '',
+        trabalhos: '',
+        notaAtitude: '',
+        atitude: ''
+    });
+
+    function controlNotasTestes(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, notasTestes: 0 });
+        } else if (value > 20) {
+            setFormData({ ...formData, notasTestes: 20 });
+        } else {
+            setFormData({ ...formData, notasTestes: value });
+        }
+    }
+    function controlTestes(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, testes: 0 });
+        } else if (value > 100) {
+            setFormData({ ...formData, testes: 100 });
+        } else {
+            setFormData({ ...formData, testes: value });
+        }
+    }
+    function controlNotasTrabalhos(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, notasTrabalhos: 0 });
+        } else if (value > 20) {
+            setFormData({ ...formData, notasTrabalhos: 20 });
+        } else {
+            setFormData({ ...formData, notasTrabalhos: value });
+        }
+    }
+    function controlTrabalhos(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, trabalhos: 0 });
+        } else if (value > 100) {
+            setFormData({ ...formData, trabalhos: 100 });
+        } else {
+            setFormData({ ...formData, trabalhos: value });
+        }
+    }
+    function controlNotaAtitude(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, notaAtitude: 0 });
+        } else if (value > 20) {
+            setFormData({ ...formData, notaAtitude: 20 });
+        } else {
+            setFormData({ ...formData, notaAtitude: value });
+        }
+    }
+
+    function controlAtitude(e) {
+        const value = e.target.value;
+        if (value < 0) {
+            setFormData({ ...formData, atitude: 0 });
+        } else if (value > 100) {
+            setFormData({ ...formData, atitude: 100 });
+        } else {
+            setFormData({ ...formData, atitude: value });
+        }
+    }
+
+    function mediaTresNotas(e) {
+        e.preventDefault();
+        const media = (parseFloat(formData.notasTestes) + parseFloat(formData.notasTrabalhos) + parseFloat(formData.notaAtitude)) / 3;
+        alert(`A média das três notas é: ${media.toFixed(2)}`);
+        if (media >= 9.5) {
+            alert('Aprovado!');
+        } else {
+            alert('Reprovado!');
+        }
+    }
+
+    function limparFormulario() {
+        setFormData({
+            nome: '',
+            disciplina: '',
+            notasTestes: '',
+            testes: '',
+            notasTrabalhos: '',
+            trabalhos: '',
+            notaAtitude: '',
+            atitude: ''
+        });
+    }
+
+
+    return (
+        <div className="mt-4 row">
+
+            <div className="w-100">
+                <h1>Cálculo da nota final</h1>
+                <h2>Dados do Aluno</h2>
+            </div>
+
+            <form className="col-12 row">
+                <div className="col-8">
+                    <div className="form-group">
+                        <label>Nome do Aluno</label>
+                        <input type="text" className="form-control" value={formData.nome} onChange={(e) =>
+                            setFormData({ ...formData, nome: e.target.value })} required />
+                    </div>
+                </div>
+
+                <div className="col-4">
+                    <div className="form-group">
+                        <label>Disciplina</label>
+                        <input type="text" className="form-control" value={formData.disciplina} onChange={(e) =>
+                            setFormData({ ...formData, disciplina: e.target.value })} required />
+
+                    </div>
+                </div>
+
+                <div className="col-3">
+                    <div className="form-group">
+                        <label>Notas dos Testes</label>
+                        <input type="number" className="form-control" value={formData.notasTestes} onChange={controlNotasTestes} required />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Notas dos Trabalhos</label>
+                        <input type="number" className="form-control" value={formData.notasTrabalhos} onChange={controlNotasTrabalhos} required />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Nota de Atitude</label>
+                        <input type="number" className="form-control" value={formData.notaAtitude} onChange={controlNotaAtitude} required />
+                    </div>
+                <button type="submit" className="btn btn-primary" onClick={mediaTresNotas}>Calcular Média</button>
+                <button type="button" className="btn btn-outline-secondary"
+                        onClick={limparFormulario}>Limpar</button>
+                </div>
+
+                <div className="col-3">
+                    <div className="form-group">
+                        <label>Testes (%)</label>
+                        <input type="number" className="form-control" value={formData.testes} onChange={controlTestes} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Trabalhos (%)</label>
+                        <input type="number" className="form-control" value={formData.trabalhos} onChange={controlTrabalhos} required />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Atitude (%)</label>
+                        <input type="number" className="form-control" value={formData.atitude} onChange={controlAtitude} required />
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Notas; 
